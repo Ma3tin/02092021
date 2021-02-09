78bfc6de83fb07f7bc98c929b557b79d246827dd
Ma3tin <martin.mares@educanet.cz>
2021-02-09 16:23:23 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Main {
@@ -25,14 +26,19 @@ public class Main {
 
         String line = "";
         try {
-            HashSet<Integer> noNameIco = new HashSet<>();
+            HashSet<String> noNameIco = new HashSet<>();
+            ArrayList<String> orderLine = new ArrayList<>();
+            BufferedWriter nf = new BufferedWriter(new FileWriter("nezname-faktury"));
+            long total = 0;
             for (int i = 2015; i <= 2020; i++) {
                 String fileName = "VF_" + i + ".dat";
                 String fileNameDone = "VF_" + i + "-done.dat";
                 BufferedReader br = new BufferedReader(new FileReader(fileName));
                 BufferedWriter bw = new BufferedWriter(new FileWriter(fileNameDone));
+                BufferedWriter ns = new BufferedWriter(new FileWriter("nezname-spolecnosti"));
                 line = br.readLine();
                 line = br.readLine();
+                int last = -1;
                 while (line != null) {
                     String[] row = line.split(",");
                     bw.write(row[0] + ";");
@@ -46,28 +52,48 @@ public class Main {
                     }
                     bw.write(name[j] + ";");
                     bw.write(row[2]);
+                    for (int k = 0; k < fileName.length(); k++) {
+                        int column = line.indexOf(",");
+                        line.substring(0, column);
+
+                    }
                     bw.newLine();
                     line = br.readLine();
-                    boolean isIn = false;
-                    for (int k = 0; k < fileName.length(); k++) {
-                        isIn = isNotInArray(row[1], ico);
-                        if (isIn) noNameIco.add(Integer.parseInt(row[1]));
+                    int actual = Integer.parseInt(row[0]);
+                    if (last != -1) {
+                        int rozdil = actual - last;
+                        if (rozdil > 1) {
+                            //System.out.println(last + 1);
+                            int missing = (last + 1);
+                            nf.write(String.valueOf(missing));
+                            nf.newLine();
+                        }
                     }
+                    last = actual;
+
+                    int money = Integer.parseInt(row[2]);
+                    total += money;
                 }
                 bw.close();
+                ArrayList<String> listIco = new ArrayList<>(noNameIco);
+                for (int j = 0; j < listIco.size(); j++) {
+                    ns.write(listIco.get(j));
+                    ns.newLine();
+                }
+                ns.close();
             }
-            System.out.println(noNameIco);
-
-
+            nf.close();
+            System.out.println(total);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static boolean isNotInArray (String rowIco, String[] ico) {
+
+    public static boolean isNotInArray(String rowIco, String[] ico) {
         for (int i = 0; i < ico.length; i++) {
-           if (ico[i].equals(rowIco)) return false;
+            if (ico[i].equals(rowIco)) return false;
 
         }
         return true;
